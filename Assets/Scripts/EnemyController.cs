c83da8ab0ce7257b48539970e979598c587c5c78
nabeelnazim <nabeelnazim4@gmail.com>
2022-03-16 15:02:08 +0500

    1st

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Transform player;
+    public Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 diretion = player.position - transform.position;
+        float angle = Mathf.Atan2(diretion.y, diretion.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        diretion.Normalize();
+        movement = diretion;
+    }
+    private void FixedUpdate()
+    {
+        movecharacter(movement);
+    }
+    void movecharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * 2 * Time.deltaTime));
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float MovX = Input.GetAxisRaw("Horizontal");
+        float MovY = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(20 * MovX, rb.velocity.y);
+        rb.velocity = new Vector2(20 * MovY, rb.velocity.x);
+    }
+}
